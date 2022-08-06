93389a8791619be524fd5128a96fcec80ab350b7
Aleksey <busido_na87@mail.ru>
2022-08-06 13:52:45 +0300

    task_27

---
+++ task_27/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Задача №27");
+Console.Write("Введи число N : ");
+int N = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while(N>0){
+    result=result + N%10;
+    N=N/10;}
+Console.WriteLine($"Cумма цифр в числе = {result}");
